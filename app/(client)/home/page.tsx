3e85261970fc7ebb96cdacd9fd62791e9d9b755e
Bhargab Dhungel <bhargabdhungel@gmail.com>
2024-05-25 16:02:40 +0530

    adds vehicle

--- app/(client)/home/page.tsx
+++ app/(client)/home/page.tsx
@@ -1,18 +1,21 @@
 import getAll from "@/app/actions/bus/getAll";
 import isPaidUser from "@/app/actions/user/isPaid";
+import { Button } from "@/components/ui/button";
 import getUserServer from "@/hooks/useAuthServer";
+import Link from "next/link";
 
 export default async function HomePage(): Promise<JSX.Element> {
   const user = await getUserServer();
   const paid = await isPaidUser();
-  const result = await getAll();
-  console.log(result);
   return (
     <div className="flex justify-center items-center h-full">
       <div>
         <h1>Welcome to TrackNGo! {user.name}</h1>
         <p>Here are the buses:</p>
         <p>{paid ? "Paid" : "Not paid"} User</p>
+        <Link href="/addvehicle">
+          <Button>Add New Vehicle</Button>
+        </Link>
       </div>
     </div>
   );
---
+++ app/actions/bus/add.ts
@@ -0,0 +1,58 @@
+"use server";
+
+import getUserServer from "@/hooks/useAuthServer";
+import prisma from "@/prisma/db";
+
+export default async function addVehicle(vehiclename: string) {
+  const token = await getUserServer();
+  const email = token.email as string;
+
+  vehiclename = vehiclename.toLocaleLowerCase();
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+    select: {
+      userId: true,
+    },
+  });
+
+  if (!user) {
+    return {
+      success: false,
+      message: "User not found",
+      description: "Please login to create add a vehicle",
+    };
+  }
+
+  const vehicleExists = await prisma.bus.findUnique({
+    where: {
+      name: vehiclename,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  if (vehicleExists) {
+    return {
+      success: false,
+      message: "Vehicle already exists",
+      description: "Please enter a different vehicle name",
+    };
+  }
+
+  const bus = await prisma.bus.create({
+    data: {
+      name: vehiclename,
+      userId: user.userId,
+    },
+  });
+
+  return {
+    success: true,
+    message: "Vehicle created successfully",
+    description: `Vehicle ${vehiclename} has been created`,
+  };
+}
