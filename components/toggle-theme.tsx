dbf2411968d3e5c8963c6a188e076b8215791e27
thergupta2001 <2001.rohangupta@gmail.com>
2024-05-31 12:06:45 +0530

    loading component color, light/dark mode

--- components/toggle-theme.tsx
+++ components/toggle-theme.tsx
@@ -1,15 +1,20 @@
 "use client";
+import { loadingAtom } from "@/atoms/loading";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
+import { useSetRecoilState } from "recoil";
 
 export default function ModeToggle(): JSX.Element {
   const { theme, setTheme } = useTheme();
+  const setLoading = useSetRecoilState(loadingAtom);
 
   function changeTheme() {
     if (theme == "dark") {
       setTheme("light");
+      setLoading("black")
     } else {
       setTheme("dark");
+      setLoading("white");
     }
   }
 
