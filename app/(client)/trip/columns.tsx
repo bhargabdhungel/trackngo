382cdb0a85739d15779cea63a0a7b8ed65e9a930
thergupta2001 <2001.rohangupta@gmail.com>
2024-06-03 17:36:28 +0530

    row action for driver details, bus details

--- app/(client)/trip/columns.tsx
+++ app/(client)/trip/columns.tsx
@@ -2,6 +2,17 @@
 
 import { Trip } from "@/lib/types";
 import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link";
 
 export const columns: ColumnDef<Trip>[] = [
     {
@@ -39,5 +50,34 @@ export const columns: ColumnDef<Trip>[] = [
     {
         accessorKey: "balance",
         header: "BALANCE"
-    }
+    },
+
+    {
+        id: "actions",
+        cell: ({ row }) => {
+            const { driverId } = row.original;
+            const { busId } = row.original;
+
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="h-8 w-8 p-0">
+                            <span className="sr-only">Open menu</span>
+                            <MoreHorizontal className="h-4 w-4" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>
+                            <Link href={`/driverDetails/${driverId}`}>View Driver details</Link>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            <Link href={`/busDetails/${busId}`}>View Bus details</Link>
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            )
+        },
+    },
 ]
