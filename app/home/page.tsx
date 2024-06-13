0476dade982c6ad47b728fe1b8dfc1c2b114c2a9
Bhargab Dhungel <bhargabdhungel@gmail.com>
2024-06-13 13:47:49 +0530

    home empty

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,31 +1,9 @@
 "use client";
-import Loading from "@/components/loading";
-import { Button } from "@/components/ui/button";
-import useAuthClient from "@/hooks/useAuthClient";
-import Link from "next/link";
-
-export default function HomePage() {
-  const { loading, userData } = useAuthClient();
-
-  if (loading) return <Loading />;
-
+export default function Home() {
   return (
-    <div className="flex justify-center items-center h-full">
-      <div>
-        <h1>Welcome to TrackNGo! {userData?.name}</h1>
-        <p>Here are the buses:</p>
-        <div className="flex space-x-4">
-          <Link href="/vehicle">
-            <Button>View Vehicles</Button>
-          </Link>
-          <Link href="/driver">
-            <Button>View Drivers</Button>
-          </Link>
-          <Link href="/trip">
-            <Button>View Trips</Button>
-          </Link>
-        </div>
-      </div>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="text-5xl animate-moveUp p-8">Priyanshu Singh</div>
+      <div className="text-3xl animate-slideIn">This page is empty for you</div>
     </div>
   );
 }
