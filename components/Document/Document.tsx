7d3a2fd09ff0ba56ab254c3248bdf043c7fb7fce
thergupta2001 <2001.rohangupta@gmail.com>
2024-06-17 00:03:25 +0530

    /driver/id design

--- components/Document/Document.tsx
+++ components/Document/Document.tsx
@@ -5,6 +5,7 @@ import { Button } from "../ui/button";
 import { format } from "date-fns";
 import Modal from "../Modal";
 import { useState } from "react";
+import { HoverDocs } from "./HoverDocs";
 
 interface DocumentListProps {
     documents: Document[];
@@ -25,50 +26,51 @@ export function DocumentList({ documents }: DocumentListProps) {
     };
 
     return (
-        <div className="flex flex-wrap justify-around mx-20">
-            {documents
-                .map((doc, index) => (
-                    <div
-                        key={index}
-                        className="relative flex flex-col items-center p-4 my-3 mx-4 sm:mx-10 border rounded-lg shadow-md hover:cursor-pointer hover:shadow-lg hover:shadow-white hover:bg-slate-900 w-full sm:w-60"
-                        style={{ height: "390px", overflow: "hidden" }}
-                    >
-                        <p className="sm:text-lg text-md font-semibold mb-2">{doc.type}</p>
-                        <div style={{ width: "100%", height: "250px", overflow: "hidden" }}>
-                            <ImageComponent
-                                src={doc.link}
-                                alt={`Document Image ${index}`}
-                                layout="responsive"
-                                width={250}
-                                height={250}
-                                className="object-contain rounded-lg shadow-md"
-                            />
+        <>
+            <div className="flex flex-wrap justify-around mx-20">
+                {documents
+                    .map((doc, index) => (
+                        <div
+                            key={index}
+                            className="relative flex flex-col items-center p-4 my-3 mx-4 sm:mx-10 border rounded-lg shadow-md hover:cursor-pointer hover:shadow-lg hover:shadow-white hover:bg-slate-900 w-full sm:w-60"
+                            style={{ height: "390px", overflow: "hidden" }}
+                        >
+                            <p className="sm:text-lg text-md font-semibold mb-2">{doc.type}</p>
+                            <div style={{ width: "100%", height: "250px", overflow: "hidden" }}>
+                                <ImageComponent
+                                    src={doc.link}
+                                    alt={`Document Image ${index}`}
+                                    layout="responsive"
+                                    width={250}
+                                    height={250}
+                                    className="object-contain rounded-lg shadow-md"
+                                />
+                            </div>
+                            <div className="absolute bottom-0 left-0 right-0 flex flex-col justify-center p-2">
+                                <Button className="mx-5" onClick={() => openModal(doc.link)}>
+                                    <span className="text-xs md:text-sm">View Document</span>
+                                </Button>
+                                {
+                                    doc?.expiryDate ? (
+                                        <>
+                                            <p className="z-10 text-center mt-2 mb-1">Expires</p>
+                                            <hr className="border-t-2 border-gray-500 mx-20" />
+                                            <p className="z-10 text-center mt-1">{format(new Date(doc.expiryDate), "dd/MM/yy")}</p>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <p className="z-10 text-center mt-2 mb-1">Expires</p>
+                                            <hr className="border-t-2 border-gray-500 mx-20" />
+                                            <p className="z-10 text-center mt-1">N/A</p>
+                                        </>
+                                    )
+                                }
+                            </div>
                         </div>
-                        <div className="absolute bottom-0 left-0 right-0 flex flex-col justify-center p-2">
-                            <Button className="mx-5" onClick={() => openModal(doc.link)}>
-                                <span className="text-xs md:text-sm">View Document</span>
-                            </Button>
-                            {
-                                doc?.expiryDate ? (
-                                    <>
-                                        <p className="z-10 text-center mt-2 mb-1">Expires</p>
-                                        <hr className="border-t-2 border-gray-500 mx-20" />
-                                        <p className="z-10 text-center mt-1">{format(new Date(doc.expiryDate), "dd/MM/yy")}</p>
-                                    </>
-                                ) : (
-                                    <>
-                                        <p className="z-10 text-center mt-2 mb-1">Expires</p>
-                                        <hr className="border-t-2 border-gray-500 mx-20" />
-                                        <p className="z-10 text-center mt-1">N/A</p>
-                                    </>
-                                )
-                            }
-                        </div>
-                    </div>
-                ))}
+                    ))}
 
-            <Modal isOpen={isModalOpen} onClose={closeModal} imageSrc={currentImage} />
-
-        </div>
+                <Modal isOpen={isModalOpen} onClose={closeModal} imageSrc={currentImage} />
+            </div>
+        </>
     );
 }
---
+++ components/Document/HoverDocs.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import {
+    HoverCard,
+    HoverCardContent,
+    HoverCardTrigger,
+} from "@/components/ui/hover-card";
+import { Button } from "@/components/ui/button";
+import { Document } from "@/lib/types";
+import { format } from "date-fns";
+import { useState } from "react";
+import Modal from "../Modal";
+
+interface DocumentListProps {
+    documents: Document[];
+}
+
+export function HoverDocs({ documents }: DocumentListProps) {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [currentImage, setCurrentImage] = useState("");
+
+    const openModal = (imageSrc: string) => {
+        setCurrentImage(imageSrc);
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+        setCurrentImage("");
+    };
+
+    return (
+        <div className="w-4/5 sm:w-4/5 md:w-1/2 lg:w-1/3 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-2 items-center justify-center">
+            {documents.map((doc, index) => (
+                <div key={index} className="flex justify-center">
+                    <HoverCard>
+                        <HoverCardTrigger asChild>
+                            <Button variant="outline" className="mt-2">
+                                {doc.type}
+                            </Button>
+                        </HoverCardTrigger>
+                        <HoverCardContent className="w-80">
+                            <div className="flex justify-between space-x-4">
+                                <div className="space-y-1">
+                                    <h4 className="text-sm font-semibold mt-1">
+                                        Expires On:
+                                    </h4>
+                                    <p className="text-sm">
+                                        {doc?.expiryDate ? (
+                                            <span className="mt-2">
+                                                {format(
+                                                    new Date(doc.expiryDate),
+                                                    "dd/MM/yy"
+                                                )}
+                                            </span>
+                                        ) : (
+                                            <span className="mt-2">N/A</span>
+                                        )}
+                                    </p>
+                                </div>
+                                <div>
+                                    <Button
+                                        onClick={() => openModal(doc.link)}
+                                    >
+                                        View
+                                    </Button>
+                                    <Button className="m-2" variant="outline">
+                                        Delete
+                                    </Button>
+                                </div>
+                            </div>
+                        </HoverCardContent>
+                    </HoverCard>
+                </div>
+            ))}
+
+            <Modal isOpen={isModalOpen} onClose={closeModal} imageSrc={currentImage} />
+        </div>
+    );
+}
