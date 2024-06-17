bd8e102858957448525c84cad3c8093a8906289a
thergupta2001 <2001.rohangupta@gmail.com>
2024-06-17 14:42:42 +0530

    /vehicle/new page design

--- components/input-bus.tsx
+++ components/input-bus.tsx
@@ -23,6 +23,14 @@ import { vehiclesAtom } from "@/atoms/vehicle";
 import Loading from "@/app/loading";
 import useFetchData from "@/hooks/useFetchData";
 import getAllVehicles from "@/app/actions/vehicle/getAll";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const FormSchema = z.object({
   vehiclename: z.string().min(2, {
@@ -52,13 +60,15 @@ export default function InputBus() {
           if (!currentVehicles) return [response.data!];
           return [...currentVehicles, response.data!];
         });
+
+        toast({
+          title: response.message,
+          description: response.description,
+        });
+
         form.reset();
         router.replace("/vehicles");
       }
-      toast({
-        title: response.message,
-        description: response.description,
-      });
     } catch (error) {
       console.error(error);
       toast({
@@ -73,24 +83,34 @@ export default function InputBus() {
   if (loading) return <Loading />;
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
-        <FormField
-          control={form.control}
-          name="vehiclename"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Vehicle</FormLabel>
-              <FormControl>
-                <Input placeholder="vehicle number" {...field} />
-              </FormControl>
-              <FormDescription>Vehicle number should be unique</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+    <Card className="w-full max-w-md mx-auto">
+      <CardHeader>
+        <CardTitle>Add a Vehicle</CardTitle>
+        <CardDescription>Add a new vehicle to the system.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              control={form.control}
+              name="vehiclename"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Vehicle Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Vehicle number" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Vehicle number should be unique
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
   );
 }
